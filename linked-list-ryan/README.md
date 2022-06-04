ec0a827d41f898e95a49cb5cd3ae4d945c152fd3
benjaminhuanghuang <bhuang@rms.com>
2022-06-03 19:45:25 -0700

    update

---
+++ linked-list-ryan/README.md
@@ -0,0 +1,5 @@
+# A Singly Linked List in Rust (2020)
+https://www.youtube.com/watch?v=IiDHTIsmUi4&t=30s&ab_channel=RyanLevick
+
+
+
---
+++ linked-list-ryan/src/lib.rs
@@ -0,0 +1,12 @@
+enum Node {
+  Empty,
+  NonEmpty(u32, Node),
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn it_works() {
+    //let node = Node::
+  }
+}
