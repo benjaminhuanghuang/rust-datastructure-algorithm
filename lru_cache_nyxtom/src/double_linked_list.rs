b5e7e5eb226e026b5c91c58dc1cef5ca4e15a985
Benjamin Huang <benjhuang@gmail.com>
2022-12-11 21:56:14 -0800

    lru cache

--- lru_cache_nyxtom/src/double_linked_list.rs
+++ lru_cache_nyxtom/src/double_linked_list.rs
@@ -30,6 +30,7 @@ type NodePtr<T> = Rc<RefCell<DoubleLinkedListNode<T>>>;
 pub struct DoubleLinkedList<T: Copy> {
   head: Option<NodePtr<T>>,
   tail: Option<NodePtr<T>>,
+  count: usize,
 }
 
 impl<T: Copy> DoubleLinkedList<T> {
@@ -37,6 +38,7 @@ impl<T: Copy> DoubleLinkedList<T> {
     DoubleLinkedList {
       head: None,
       tail: None,
+      count: 0,
     }
   }
 
@@ -56,6 +58,7 @@ impl<T: Copy> DoubleLinkedList<T> {
         }
       }
     }
+    self.count += 1;
   }
 
   pub fn push_back(&mut self, value: T) {
@@ -72,6 +75,7 @@ impl<T: Copy> DoubleLinkedList<T> {
         current_tail.borrow_mut().next = self.tail.clone();
       }
     }
+    self.count += 1;
   }
 
   pub fn pop_back(&mut self) -> Option<T> {
@@ -92,6 +96,7 @@ impl<T: Copy> DoubleLinkedList<T> {
             }
           }
         };
+        self.count -= 1;
         Some(tail.value)
       }
     }
@@ -112,9 +117,70 @@ impl<T: Copy> DoubleLinkedList<T> {
             self.head = Some(next);
           }
         };
+        self.count -= 1;
         Some(head.value)
       }
     }
+  }
+  pub fn move_node_to_back(&mut self, mut node: NodePtr<T>) {
+    self.remove_node(&mut node);
+    self.push_node_back(node);
+  }
+
+  pub fn remove_node(&mut self, node: &mut NodePtr<T>) {
+    let (prev, next) = {
+      let mut node = node.borrow_mut();
+      let prev = match node.prev.take() {
+        None => None,
+        Some(prev) => prev.upgrade(),
+      };
+      let next = node.next.take();
+      (prev, next)
+    };
+
+    match (prev, next) {
+      (None, None) => {
+        self.head = None;
+        self.tail = None;
+      }
+      (None, Some(next)) => {
+        next.borrow_mut().prev = None;
+        self.head.replace(next);
+      }
+      (Some(prev), None) => {
+        prev.borrow_mut().prev = None;
+        self.tail.replace(prev);
+      }
+      (Some(prev), Some(next)) => {
+        next.borrow_mut().prev.replace(Rc::downgrade(&prev));
+        prev.borrow_mut().next.replace(next);
+      }
+    }
+  }
+
+  pub fn push_node_back(&mut self, node: NodePtr<T>) {
+    match self.tail.take() {
+      None => {
+        self.head.replace(node);
+        self.tail = self.head.clone();
+      }
+      Some(current_tail) => {
+        node.borrow_mut().prev.replace(Rc::downgrade(&current_tail));
+        self.tail.replace(node);
+        current_tail.borrow_mut().next = self.tail.clone();
+      }
+    }
+  }
+
+  pub fn get_weak_tail(&self) -> Option<Weak<RefCell<DoubleLinkedListNode<T>>>> {
+    match &self.tail {
+      None => None,
+      Some(tail) => Some(Rc::downgrade(tail)),
+    }
+  }
+
+  pub fn len(&self) -> usize {
+    self.count
   }
 }
 
